650c3ad683127ab6b9fab66d439feb3a13d84785
Alex_Black <37541820+perekis228@users.noreply.github.com>
2024-09-22 19:23:34 +0200

    Delete 18_2/UrbanDjango/task2/18_3 directory

--- 18_2/UrbanDjango/task2/18_3/UrbanDjango/task2/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-def classT(request):
-    return render(request, 'second_task/class_template.html')
-
-def funcT(request):
-    return render(request, 'second_task/func_template.html')
